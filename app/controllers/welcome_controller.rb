f03c774ae1d6cbcb7dc98aa984e30985ec99bbeb
Richard Blankenhorn <rsbnq6@yahoo.com>
2017-08-09 22:27:47 -0600

    add new resume

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,7 +10,7 @@ class WelcomeController < ApplicationController
 
   def download_pdf
     send_file(
-      "#{Rails.root}/public/RichardBlankenhornResume.pdf",
+      "#{Rails.root}/public/Resume.pdf",
       filename: "Resume.pdf",
       type: "application/pdf"
     )
